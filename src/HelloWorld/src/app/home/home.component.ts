f7b0e3ebdd60e2bcab21ec94f37f15caa7f917aa
asaf <asaf.gunay@serim.com.tr>
2018-11-08 18:11:22 +0300

    1. adım elmayı home.component'te çağır

--- src/HelloWorld/src/app/home/home.component.ts
+++ src/HelloWorld/src/app/home/home.component.ts
@@ -1,17 +1,14 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
-    selector: "Home",
-    moduleId: module.id,
-    templateUrl: "./home.component.html"
+  selector: "my-app",
+  template: `
+    <ActionBar title="My App" class="action-bar"></ActionBar>
+    <image
+      src="https://user-images.githubusercontent.com/544280/42960643-66d498ac-8b5a-11e8-8946-7224eefea6a5.jpg"
+    ></image>
+  `
 })
-export class HomeComponent implements OnInit {
-
-    constructor() {
-        // Use the component constructor to inject providers.
-    }
-
-    ngOnInit(): void {
-        // Init your component properties here.
-    }
+export class HomeComponent {
+  // Your TypeScript logic goes here
 }
