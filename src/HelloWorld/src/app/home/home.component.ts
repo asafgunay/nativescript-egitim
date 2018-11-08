2dd263bdf6dbc0e6977a11245193f1dcf3fb8546
asaf <asaf.gunay@serim.com.tr>
2018-11-08 18:59:17 +0300

    show error log on terminal

--- src/HelloWorld/src/app/home/home.component.ts
+++ src/HelloWorld/src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "my-app",
   template: `
-    <ActionBar title="My App" class="action-bar"></ActionBar>
+    <ActionBar title="My App" class="action-bar"></ActionBar
     <image
       src="https://user-images.githubusercontent.com/544280/42960643-66d498ac-8b5a-11e8-8946-7224eefea6a5.jpg"
     ></image>
